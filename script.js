40bb4bc12f878676dc4a6db43d06be26aab37469
CHIKHAOUI Amel <amel.chk04@gmail.com>
2025-03-06 11:16:01 +0100

    V1_a_jour

--- script.js
+++ script.js
@@ -8,14 +8,26 @@ document.addEventListener('DOMContentLoaded', () => {
             e.preventDefault();
             const targetId = link.getAttribute('href').substring(1);
 
-            // Masquer toutes les sections
-            sections.forEach(section => {
-                section.classList.remove('active');
-            });
+            // Si le lien "Accueil" est cliqué
+            if (targetId === 'accueil') {
+                // Afficher les deux sections "section1" et "section2" 
+                document.getElementById('section1').classList.add('active');
+                document.getElementById('section2').classList.add('active');
+                document.getElementById('apropos').classList.remove('active')
+            } else {
+                // Pour les autres liens, on masque toutes les sections
+                sections.forEach(section => {
+                    section.classList.remove('active');
+                });
 
-            // Afficher la section ciblée
-            document.getElementById(targetId).classList.add('active');
+                // Afficher la section liée au lien cliqué
+                const targetSection = document.getElementById(targetId);
+                if (targetSection) {
+                    targetSection.classList.add('active');
+                }
+            }
         });
     });
-
 });
+
+
